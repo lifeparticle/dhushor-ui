b2d2aef5ba18749d8468156a53973fa443c49113
Al-Junaed Islam <brohi96@gmail.com>
2021-08-29 12:31:20 +0600

    links css

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,38 +1,40 @@
-import { FaBeer } from "react-icons/fa";
+import { FaPen, FaEtsy, FaBook, FaCuttlefish } from "react-icons/fa";
+import { GoLogoGist } from "react-icons/go";
+
 export const NOOPENER_NOREFERRER = "noopener,noreferrer";
 export let links = [
 	{
 		title: "Gist Github",
 		description: "this description",
-		cover: FaBeer,
+		cover: GoLogoGist,
 		link: "https://gist.github.com/",
 		enable: true,
 	},
 	{
 		title: "ProWritingAid",
 		description: "this description",
-		cover: FaBeer,
+		cover: FaPen,
 		link: "https://prowritingaid.com/Free",
 		enable: true,
 	},
 	{
 		title: "Grammarly",
 		description: "this description",
-		cover: FaBeer,
+		cover: FaBook,
 		link: "https://app.grammarly.com/docs/new",
 		enable: true,
 	},
 	{
 		title: "Hemingway Editor",
 		description: "this description",
-		cover: FaBeer,
+		cover: FaEtsy,
 		link: "http://www.hemingwayapp.com/",
 		enable: true,
 	},
 	{
 		title: "Carbon Now Sh",
 		description: "this description",
-		cover: FaBeer,
+		cover: FaCuttlefish,
 		link: "https://carbon.now.sh/",
 		enable: true,
 	},
