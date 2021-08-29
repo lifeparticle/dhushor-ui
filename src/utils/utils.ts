3d920d51c72ec5812f9c43d8c805214b7fc008d4
lifeparticle <mahbubzaman17@gmail.com>
2021-08-29 17:49:35 +1000

    tidy

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,7 @@ export const openInNewTab = (url: string, tab: string = "_blank") => {
 	const newWindow = window.open(url, tab, NOOPENER_NOREFERRER);
 	if (newWindow) newWindow.opener = null;
 };
+
+export const convertCase = (text: string) => {
+	return "";
+};
