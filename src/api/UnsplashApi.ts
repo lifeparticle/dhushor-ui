77f98df0ab68b3efa7340418541368cdc8c4fd90
Al-Junaed Islam <brohi96@gmail.com>
2021-09-29 18:49:19 +0600

    api

--- src/api/UnsplashApi.ts
+++ src/api/UnsplashApi.ts
@@ -1,1 +1,75 @@
-export {};
+import { createApi } from "unsplash-js";
+import { httpGet } from "./Config";
+
+export interface UnsplashInterface {
+    urls: string;
+}
+
+class UnsplashApi {
+    clientId = "7O6rhxCZTJUm_tJ2nFhwAlbCXo_oGfxkcrn8LzPNPVU";
+
+    getImage = (query: string) => {
+        const url = `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${this.clientId}`;
+
+        const test = httpGet(url)
+        // console.log(test);
+
+        return test;
+    };
+}
+
+export default UnsplashApi;
+
+
+
+
+
+
+// const onSearch = (query: string) => {
+//     unsplash.search
+//         .getPhotos({
+//             query: query,
+            // page: 1,
+            // perPage: 10,
+            // color: 'green',
+            // orientation: 'portrait',
+//         })
+//         .then(result => {
+//             if (result.errors) {
+                // handle error here
+//                 console.log('error occurred: ', result.errors[0]);
+//             } else {
+                // handle success here
+//                 const photos = result.response.results;
+//                 console.log(photos);
+                // setPhotos(photos);
+//             }
+//         });
+// };
+//     <Image.PreviewGroup>
+//     {
+//         Object.entries(photos).map(item => {
+//         })
+//     }
+// </Image.PreviewGroup>
+
+
+// const [photos, setPhotos] = useState<any[]>([]);
+
+// useEffect(() => {
+    // setData(new PexelsApi.getImage());
+//     const url = `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${clientId}`;
+
+//     axios.get(url).then((response) => {
+        // console.log(response);
+//         setPhotos(response.data.results);
+//     });
+// });
+
+
+
+// const onSearch = (query: string) => {
+
+
+//     
+// };
