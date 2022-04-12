287dce33c1321fb5ce55a783f8a18ee706bf9e23
md-ashikul-islam <md.ashikul.islam@g.bracu.ac.bd>
2022-04-12 16:51:58 +0600

    Fixed Search Bar titles Unsplashed to Pexels

--- src/components/SearchImage/SearchImage.tsx
+++ src/components/SearchImage/SearchImage.tsx
@@ -2,33 +2,35 @@ import { Input } from "antd";
 const { Search } = Input;
 
 interface SearchImageprops {
-	setSearchedValue: any;
-	onImageSearch: any;
-	isLoading: boolean;
-	photos: any[];
+  setSearchedValue: any;
+  onImageSearch: any;
+  isLoading: boolean;
+  photos: any[];
+  placeHolder: string;
 }
 
 export const SearchImage: React.FC<SearchImageprops> = ({
-	setSearchedValue,
-	onImageSearch,
-	isLoading,
-	photos,
+  setSearchedValue,
+  onImageSearch,
+  isLoading,
+  photos,
+  placeHolder,
 }) => {
-	return (
-		<>
-			<Search
-				onChange={(e) => setSearchedValue(e.target.value)}
-				enterButton
-				placeholder="Unsplash"
-				onSearch={(value) => {
-					onImageSearch(value, true);
-				}}
-				size="large"
-				loading={isLoading}
-			/>
-			<br />
-			<br />
-			<br />
-		</>
-	);
+  return (
+    <>
+      <Search
+        onChange={(e) => setSearchedValue(e.target.value)}
+        enterButton
+        placeholder={placeHolder}
+        onSearch={(value) => {
+          onImageSearch(value, true);
+        }}
+        size="large"
+        loading={isLoading}
+      />
+      <br />
+      <br />
+      <br />
+    </>
+  );
 };
